5be63b468c9fc158c74b2868f3e9e843ba92200c
Abe Nelson <29802261+abnlsn@users.noreply.github.com>
2026-04-22 13:28:44 -0400

    Added verification/generation commands

--- matrix_mpy.py
+++ matrix_mpy.py
@@ -2,34 +2,61 @@
 # input: 2 random 8 by 8 matrix
 
 import numpy as np
+import argparse
 
 # Generate random matrix
 
-matrixOne_file = "matrix1.txt"
-matrixTwo_file = "matrix2.txt"
+def calculate_result(matrix1, matrix2):
+    return np.matmul(matrix1, matrix2)
 
-matrixOne = np.random.randint(0, 255, size=(8, 8))
-with open(matrixOne_file, "w") as f:
-    #print(matrixOne.flatten())
-    f.write("\n".join(str(v) for v in matrixOne.flatten()))
 
-matrixTwo = np.random.randint(0, 255, size=(8, 8))
-with open(matrixTwo_file, "w") as f:
-    #print(matrixTwo.flatten())
-    f.write("\n".join(str(v) for v in matrixTwo.flatten()))
+def write_random_matrix(filename):
+    matrixOne = np.random.randint(0, 255, size=(8, 8))
+    with open(filename, "w") as f:
+        f.write("\n".join(hex(v) for v in matrixOne.flatten()))
 
-# Calculate reference matrix
-result = np.matmul(matrixOne, matrixTwo)
-#print(result)
+def file_to_matrix(filename):
+    try:
+        with open(filename) as f:
+            values = []
+            for line in f:
+                values.append(int(line, base=16))
 
-# Output text file of inputs and results
+            assert len(values) == 64
 
-errors = []
+            return np.array(values).reshape(8, 8)
+    except:
+        exit(f"Couldn't open file '{filename}'")
+
 
 def verify(matrixRTL, matrixPy):
+    errors = []
     for (i, j), elemRTL in np.ndenumerate(matrixRTL):
         elemPy = matrixPy[i, j]
         if elemRTL != elemPy:
             errors.append(f"[{i},{j}] RTL is {elemRTL} but PY is {elemPy}")
 
     print(errors)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--generate", action="store_true", default=False)
+    parser.add_argument("--verify", action="store_true", default=False)
+    parser.add_argument("--matrix-a", "-a", default="matrix1.txt", help="Filename for input matrix a")
+    parser.add_argument("--matrix-b", "-b", default="matrix2.txt", help="Filename for input matrix b")
+    parser.add_argument("--result-matrix", "-c", default="output.txt", help="Filename for result matrix to verify")
+
+    args = parser.parse_args()
+
+    if args.generate:
+        print("Generating files...")
+        write_random_matrix(args.matrix_a)
+        write_random_matrix(args.matrix_b)
+    elif args.verify:
+        matrix1 = file_to_matrix(args.matrix_a)
+        matrix2 = file_to_matrix(args.matrix_b)
+        matrix_to_test = file_to_matrix(args.result_matrix)
+        result = calculate_result(matrix1, matrix2)
+        verify(matrix_to_test, result)
+    else:
+        parser.print_help()
