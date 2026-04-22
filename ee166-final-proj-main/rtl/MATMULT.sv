74cdaa12c358f8776292fb702e6a1263953d0476
0xFFFFFF1D <22594415+0xFFFFFF1D@users.noreply.github.com>
2026-04-22 13:21:46 -0400

    Renamed port names in MATMULT to match testbench

--- ee166-final-proj-main/rtl/MATMULT.sv
+++ ee166-final-proj-main/rtl/MATMULT.sv
@@ -2,14 +2,14 @@
 module MATMULT (
     input
       logic [2:0] ctrl,
-      logic signed [7:0] a1, a2, b1, b2,
+      logic signed [7:0] a1_in, a2_in, b1_in, b2_in,
       logic CLK, EN, RSTN,
     output 
-      logic signed [7:0] c1, c2, c3, c4,
+      logic signed [7:0] c1_in, c2_in, c3_in, c4_in,
       logic done
 );
 
-    logic unsigned [5:0] a_idx, b_idx, c_idx;
+    logic unsigned [6:0] a_idx, b_idx, c_idx;
 
     logic signed [7:0] a_mat [63:0];
     logic signed [7:0] b_mat [63:0];
@@ -59,7 +59,7 @@ module MATMULT (
         if (!RSTN) begin
             c_idx <= '0;
         end else begin
-          if (ctrl[2]) begin
+          if (ctrl[2] && ~done) begin
                 c1 <= c_mat[c_idx];
                 c2 <= c_mat[c_idx+1];
                 c3 <= c_mat[c_idx+2];
@@ -68,6 +68,8 @@ module MATMULT (
             end
         end
     end
+
+    always_comb done = (c_idx == '64);
 
 
     
